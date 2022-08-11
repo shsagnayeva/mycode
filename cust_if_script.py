71d9b93ddebc2460fcde113d318af4184d491070
shsagnayeva <shsagnayeva@users.noreply.github.com>
2022-08-11 17:35:22 +0000

    writing custom code

---
+++ cust_if_script.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+def main():
+
+    result = "Hurricane category is "
+
+    speed = int(input("Please enter wind speed (mph)")
+    
+    value_type = type(speed)
+    
+    if value_type = "int":    
+    
+        if speed >= 157:     
+            result = result + "five"
+        elif speed >= 130 and speed <= 156:
+            result = result + "four"
+        elif speed >= 111 and speed <= 129:
+            result = result + "three"
+        elif speed >= 96 and speed <= 110:
+            result = result + "two"
+        elif speed >= 74 and spped <= 95:
+            result = result + "one"
+        elif speed < 74:
+            result = "Not a hurricane"
+        else:
+            result = "Please provide wind speed"
+    
+    else:
+        result = "Please provide integer input"
+    
+        print(result)
+
+main()
