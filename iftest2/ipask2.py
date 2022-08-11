3e3bd5a5c1e988702a68341198805740dd9035e3
shsagnayeva <shsagnayeva@users.noreply.github.com>
2022-08-11 16:42:55 +0000

    if, elif, else

---
+++ iftest2/ipask2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# input
+ipchk = input("Apply an IP address: ") 
+
+# if user set IP of gateway
+if ipchk == "192.168.70.1":
+    print("Looks like the IP address of the Gateway was set: " + ipchk + " This is not recommended.")
+
+elif ipchk: # if any data is provided, this will test true
+    print("Looks like the IP address was set: " + ipchk)
+
+else: # if data is NOT provided
+    print("You did not provide input.")
+
