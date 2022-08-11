0062dda624773f1e1a5ccc9f96e798531b3d3234
shsagnayeva <shsagnayeva@users.noreply.github.com>
2022-08-11 16:21:49 +0000

    learnig conditional if

---
+++ iftest/condition03.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# input
+hostname = input("What value should we set for hostname? ")
+
+## str.lower() method to return a lowercase string
+if hostname.lower() == "mtg":
+
+    print("The hostname was found to be mtg")
+    print("The hostname matches expected config")
+
+print("Exiting the script")
+
