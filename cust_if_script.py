40b130cf6b10d715c6e86982cb411a2ddd4e8f99
shsagnayeva <shsagnayeva@users.noreply.github.com>
2022-08-11 18:35:27 +0000

    writing custom code

--- cust_if_script.py
+++ cust_if_script.py
@@ -3,24 +3,34 @@
 def main():
 
     result = "Hurricane category is "
-
-    speed = int(input("Please enter wind speed (mph): "))
-     
-    if speed >= 157:     
-       result = result + "five"
-    elif speed >= 130 and speed <= 156:
-       result = result + "four"
-    elif speed >= 111 and speed <= 129:
-       result = result + "three"
-    elif speed >= 96 and speed <= 110:
-       result = result + "two"
-    elif speed >= 74 and speed <= 95:
-       result = result + "one"
-    elif speed < 74:
-       result = "Not a hurricane"
-    else:
-       result = "Please provide wind speed"
     
-    print(result)
+    while True:
+
+        speed = input("Please enter wind speed (mph): ")
+        
+        try:
+
+            speed = float(speed)
+            
+            if speed >= 157:     
+                result = result + "five"
+            elif speed >= 130 and speed <= 156:
+                result = result + "four"
+            elif speed >= 111 and speed <= 129:
+                result = result + "three"
+            elif speed >= 96 and speed <= 110:
+                result = result + "two"
+            elif speed >= 74 and speed <= 95:
+                result = result + "one"
+            elif speed >= 0 and speed < 74:
+                result = "Not a hurricane"
+            else:
+                result = "Please provide positive number"
+            print(result)
+            break
+
+        except ValueError:
+            
+            print("Please enter number")
 
 main()
