1d7f55d79aa30db87c45cc8f280eddac76670fd7
shsagnayeva <shsagnayeva@users.noreply.github.com>
2022-08-15 17:39:44 +0000

    learning pylint

---
+++ pylint/smallscript.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""
+Author: Test
+
+This program prints some things to the screen
+"""
+
+def main():
+    """Main program"""
+    mynote = "print this string" # define mynote
+    print(mynote)                # display our string
+
+if __name__ == "__main__":
+    main()
+
+
