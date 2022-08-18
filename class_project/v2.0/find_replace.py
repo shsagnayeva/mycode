2ae14b1fe66b73904f9ff8748d5febcda712fc42
shsagnayeva <shsagnayeva@users.noreply.github.com>
2022-08-18 15:49:25 +0000

    flask apps

--- class_project/v2.0/find_replace.py
+++ class_project/v2.0/find_replace.py
@@ -1,34 +1,47 @@
 #!/usr/bin/python3
 
 
-"""This programs performs count words in a file, count specific word that user inputs, find and replace"""
+"""This programs performs count words in a file, 
+count specific word that user inputs, find and replace"""
 
 
 import re
 import random
 from time import sleep
-# TODO Add comments
 
 
 # Get user's input
 def get_input():
+    """get input"""
     word_to_replace = str(input("\nPlease enter word to replace: \n(press 'q' for exit) \n>>> "))
     word_to_replace = word_to_replace.lower()
     return word_to_replace
 
 
-# Count total nummber of words in a file
-def count_words():
+# Read a file
+def read_file():
+    """read a file function, lower*( and remove special characters"""
+    data = []
     with open("text.txt", "r") as file:
-        count = 0
         for line in file:
             for word in line.split():
-                count +=1
-    return count
+                data.append(word.lower())
+    res = [''.join(i for i in string if i.isalnum()) for string in data]
+    return res
+
+# Count total number of words in a file
+def count_words():
+    """count total words"""
+    with open("text.txt", "r") as file:
+        data = file.read()
+        words = data.split()
+        total_words = len(words)
+    return total_words
 
 
 # Count words that should be replaced
 def count_word_to_replace(word_to_replace):
+    """count word to be replaced"""
     with open("text.txt", "r") as file:
         count = 0
         for line in file:
@@ -40,12 +53,9 @@ def count_word_to_replace(word_to_replace):
         return count
 
 
-# TODO Check to replace only whole words, instead of parts
-# TODO Implement delete functionality
-
-
 # Replace word and update a file with replacement
 def replace_word(word_to_replace):
+    """replace word with random number of asterics and save updates"""
     with open("text.txt", "r+") as file:
         data = file.read()
         text = re.compile(re.escape(word_to_replace), re.IGNORECASE)
@@ -55,42 +65,34 @@ def replace_word(word_to_replace):
         file.write(file_content)
 
 
-# Read a file
-def read_file():
-    data = []
-    with open("text.txt", "r") as file:
-        for line in file:
-            for word in line.split():
-                data.append(word.lower())
-    res = [''.join(i for i in string if i.isalnum()) for string in data]
-    return res
-
-
 # Main function: while loop and options
 def main():
+    """runtime function"""
     total_words = count_words()
     print(f"\nTotal number of words in this file: {total_words}")
 
-    while(True):
-        data = read_file()
+    while True:
         user_input = get_input()
         count = count_word_to_replace(user_input)
-
+        
         if user_input == "q":
             print("Close program...")
             break
 
-        elif count == 0:
-            print(f"No '{user_input}' word in this file")
-
-        elif user_input in data:
-            print(f"{count} '{user_input}' word(s) will be replaced...\n...Replace in progress...\n")
-            replace_word(user_input)
-            sleep(2)
-            print(f"{count} '{user_input}' successcully replaced...")
-
         else:
-            print(f"No '{user_input}' word in this file")
+            data = read_file()
+
+            if count == 0:
+                print(f"No '{user_input}' word in this file")
+
+            elif user_input in data:
+                print(f"{count} '{user_input}' word(s) will be replaced...\n...")
+                replace_word(user_input)
+                sleep(2)
+                print(f"{count} '{user_input}' successcully replaced...")
+
+            else:
+                print(f"No '{user_input}' word in this file")
 
 
 if __name__ == "__main__":
