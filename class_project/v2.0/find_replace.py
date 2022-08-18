5eea153fe6fe6df96debb0e8a9977cca503383b2
shsagnayeva <shsagnayeva@users.noreply.github.com>
2022-08-18 16:13:28 +0000

    updated v2.0

--- class_project/v2.0/find_replace.py
+++ class_project/v2.0/find_replace.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 
-"""This programs performs count words in a file, 
-count specific word that user inputs, find and replace"""
+"""This programs performs count words, count specific word that user inputs, find and replace"""
 
 
 import re
@@ -18,7 +17,7 @@ def get_input():
     return word_to_replace
 
 
-# Read a file
+# Read a file, lowercase and remove special characters
 def read_file():
     """read a file function, lower*( and remove special characters"""
     data = []
@@ -28,6 +27,7 @@ def read_file():
                 data.append(word.lower())
     res = [''.join(i for i in string if i.isalnum()) for string in data]
     return res
+
 
 # Count total number of words in a file
 def count_words():
@@ -49,7 +49,7 @@ def count_word_to_replace(word_to_replace):
                 word_lowercase = word.lower()
                 word_only = re.sub(r"[^a-zA-Z0-9]", "", word_lowercase)
                 if word_only == word_to_replace.lower():
-                    count +=1
+                    count += 1
         return count
 
 
@@ -65,34 +65,39 @@ def replace_word(word_to_replace):
         file.write(file_content)
 
 
+# Delete word and update a file
+def delete_word(word_to_delete):
+    """delete word from a file and save updates"""
+    with open("text.txt", "r+") as file:
+        data = file.read()
+        text = re.compile(re.escape(word_to_delete), re.IGNORECASE)
+        file_content = text.sub("", data)
+        file.seek(0)
+        file.truncate()
+        file.write(file_content)
+
+
 # Main function: while loop and options
 def main():
     """runtime function"""
     total_words = count_words()
     print(f"\nTotal number of words in this file: {total_words}")
-
     while True:
         user_input = get_input()
+        data = read_file()
         count = count_word_to_replace(user_input)
-        
         if user_input == "q":
             print("Close program...")
             break
-
+        if count == 0:
+            print(f"No '{user_input}' word in this file")
+        elif user_input in data:
+            print(f"{count} '{user_input}' word(s) will be replaced...\n...")
+            replace_word(user_input)
+            sleep(2)
+            print(f"{count} '{user_input}' successcully replaced...")
         else:
-            data = read_file()
-
-            if count == 0:
-                print(f"No '{user_input}' word in this file")
-
-            elif user_input in data:
-                print(f"{count} '{user_input}' word(s) will be replaced...\n...")
-                replace_word(user_input)
-                sleep(2)
-                print(f"{count} '{user_input}' successcully replaced...")
-
-            else:
-                print(f"No '{user_input}' word in this file")
+            print(f"No '{user_input}' word in this file")
 
 
 if __name__ == "__main__":
